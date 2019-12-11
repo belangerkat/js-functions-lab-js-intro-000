7b891fc739f6b7c0322b1ebe8905097b106a6c65
Katie Belanger <belangerkat@gmail.com>
2019-12-11 23:56:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,10 +7,16 @@ function happyHolidaysTo(name) {
   console.log(`Happy holidays, ${name}!`)
 }
 
+happyHolidaysTo('you')
+
 function happyHolidayTo(holiday, name) {
   console.log(`Happy ${holiday}, ${name}!`)
 }
 
+happyHolidayTo("Independence Day", "you")
+
 function holidayCountdown(days, holiday) {
-  console.log(`It's ${days} days until ${holiday}!`)
+  console.log(`It\'s ${days} days until ${holiday}!`)
 }
+
+holidayCountdown(20, "Mother\'s Day")
